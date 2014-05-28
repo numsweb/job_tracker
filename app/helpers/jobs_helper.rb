4cb282b1588015e58f8639fee986165b0eca0f18
John Kropka <s2000.coder@gmail.com>
2014-05-28 10:50:45 -0500

    updating status names and classes, clean up hte helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,21 +1,6 @@
 module JobsHelper
 
   def set_class(job)
-    case Status.find(job.status).name
-      when "Deactivated"
-        "deactivated"
-      when "Waiting Feedback"
-        "waiting"
-      when "New"
-        "new"
-      when "Submitting Info"
-        "submitting"
-      when "Call Screen Scheduled"
-        "screen-scheduled"
-      when "Interview Scheduled"
-        "interview-scheduled"
-      when "Dead Lead"
-        "dead"
-    end
+    Status.find(job.status).name.gsub(" ", "-").downcase
   end
 end
