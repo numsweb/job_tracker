b631a6901215794cf0ff98aedda046d3a68c380f
numsweb <numsweb@users.noreply.github.com>
2015-09-07 13:44:19 -0500

    tweaks and styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'jobs#index'
+  root to: 'jobs#index'
 
   get 'jobs_all/:order' => 'jobs#index'
   
