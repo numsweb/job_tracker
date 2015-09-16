fa05083f72a561734e14134b26fb0c2859c5ce91
numsweb <numsweb@users.noreply.github.com>
2015-09-16 14:13:14 -0500

    show total counts in the status links

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -3,4 +3,8 @@ module JobsHelper
   def set_class(job)
     Status.find(job.status).name.gsub(" ", "-").downcase
   end
+  
+  def get_job_count(status)
+    Job.where(status_id: status.id).count
+  end
 end
