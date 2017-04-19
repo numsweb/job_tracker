dd786fb6a0e4ad413970a34df74cb7238f367162
numsweb <numsweb@users.noreply.github.com>
2017-04-18 19:54:04 -0500

    add heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.default_url_options = { :host => 'secure-scrubland-86642.herokuapp.com' }
+
+  #https://secure-scrubland-86642.herokuapp.com/jobs
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
