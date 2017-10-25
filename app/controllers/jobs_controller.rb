481c6a1fc250aace97328a9ba60694777e0c7613
numsweb <numsweb@users.noreply.github.com>
2017-10-25 16:16:26 -0400

    order by status

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,7 +18,7 @@ class JobsController < ApplicationController
     end
 
     @jobs = Job.search(params.merge(:sort_order => session[:sort_order], :filter => params[:filter]))
-    @statuses = Status.all.order("id ASC")
+    @statuses = Status.all.order("status ASC")
     respond_to do |format|
       format.html { render :index}
       format.json {render @jobs.to_json, status: :ok}
